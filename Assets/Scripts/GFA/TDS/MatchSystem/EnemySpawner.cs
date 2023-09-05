5959dc44992570226a745cc359edd997d6cf6e65
Emre <112270755+emrrekus@users.noreply.github.com>
2023-09-05 22:00:32 +0300

    Enemyspawn

--- Assets/Scripts/GFA/TDS/MatchSystem/EnemySpawner.cs
+++ Assets/Scripts/GFA/TDS/MatchSystem/EnemySpawner.cs
@@ -8,11 +8,12 @@ namespace GFA.TDS.MatchSystem
     {
         private Camera _camera;
 
+        private Plane _plane = new Plane(Vector3.up, Vector3.zero);
 
         private void Awake()
         {
             _camera = Camera.main;
-            GameObject.FindGameObjectWithTag("MainCamera");
+            
         }
 
         private void Start()
@@ -25,8 +26,17 @@ namespace GFA.TDS.MatchSystem
             while (true)
             {
                 yield return new WaitForSeconds(1);
-                var vie
-                _camera.ViewportToWorldPoint()
+                var viewportPoint = new Vector3(-0.2f, UnityEngine.Random.value);
+                var ray = _camera.ViewportPointToRay(viewportPoint);
+
+                if (_plane.Raycast(ray, out float enter))
+                {
+                    var worldPosition=ray.GetPoint(enter);
+                    var inst = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                    inst.transform.position = worldPosition;
+                }
+
+                
             }
         }
     }

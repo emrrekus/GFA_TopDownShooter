f94ab8a0f5674c210b1ff0d506ec0b344fcb012f
Emre <112270755+emrrekus@users.noreply.github.com>
2023-09-06 11:59:27 +0300

    Object pool create

--- Assets/Scripts/GFA/TDS/MatchSystem/EnemySpawner.cs
+++ Assets/Scripts/GFA/TDS/MatchSystem/EnemySpawner.cs
@@ -13,14 +13,23 @@ namespace GFA.TDS.MatchSystem
         private Plane _plane = new Plane(Vector3.up, Vector3.zero);
 
         [SerializeField] private float _offset;
+        [SerializeField] private float _spawnRate;
 
-        [SerializeField]
-        private MatchInstance _matchInstance;
+        [SerializeField] private MatchInstance _matchInstance;
 
         [SerializeField] private EnemySpawnData _enemySpawnData;
+
+        private GameObject[] _pooledObjects;
+        
         private void Awake()
         {
             _camera = Camera.main;
+            CreatePooledObjects();
+        }
+
+        private void CreatePooledObjects()
+        {
+           
         }
 
         private void Start()
@@ -33,51 +42,57 @@ namespace GFA.TDS.MatchSystem
             return vector * sign * _offset;
         }
 
-        private GameObject GetSpawnObject()
-        {
-            var time = _matchInstance.Time;
-            if (_enemySpawnData.TryGetEntryByTime(time, out SpawnEntry entry))
-            {
-                return entry.Prefabs[UnityEngine.Random.Range(0, entry.Prefabs.Length)];
-            }
 
-            return null;
-        }
-        
         private IEnumerator CreateEnemy()
         {
             while (true)
             {
                 yield return new WaitForSeconds(0.5f);
-                var viewportPoint = Vector3.zero;
+                if (!_enemySpawnData.TryGetEntryByTime(_matchInstance.Time, out SpawnEntry entry)) continue;
+                var spawnPerCall = ((float)entry.SpawnCount / entry.Duration) * _spawnRate;
+              
 
-                var offset = Vector3.zero;
+                    for (int i = 0; i < spawnPerCall; i++)
+                    {
+                        var viewportPoint = GetViewportPoint(out var offset);
 
-                if (UnityEngine.Random.value > 0.5f)
-                {
-                    var dir = Mathf.Round(UnityEngine.Random.value);
-                    viewportPoint = new Vector3(dir, UnityEngine.Random.value);
+                        var ray = _camera.ViewportPointToRay(viewportPoint);
 
-                    offset = GetSpawnOffsetByViewportPosition(Vector3.right, dir < 0.001f ? -1f : 1f);
-                }
-                else
-                {
-                    var dir = Mathf.Round(UnityEngine.Random.value);
-                    viewportPoint = new Vector3(UnityEngine.Random.value, dir);
-
-                    offset = GetSpawnOffsetByViewportPosition(Vector3.forward, dir < 0.001f ? -1f : 1f);
-                }
-
-
-                var ray = _camera.ViewportPointToRay(viewportPoint);
-
-                if (_plane.Raycast(ray, out float enter))
-                {
-                    var worldPosition = ray.GetPoint(enter) + offset;
-                    var inst = Instantiate(GetSpawnObject(), worldPosition,quaternion.identity);
-                    inst.transform.position = worldPosition;
-                }
+                        ;
+                        if (_plane.Raycast(ray, out float enter))
+                        {
+                            var objSpawn = entry.Prefabs[UnityEngine.Random.Range(0, entry.Prefabs.Length)];
+                            var worldPosition = ray.GetPoint(enter) + offset;
+                            var inst = Instantiate(objSpawn, worldPosition, quaternion.identity);
+                            inst.transform.position = worldPosition;
+                        }
+                    }
+                
             }
+        }
+
+        private Vector3 GetViewportPoint(out Vector3 offset)
+        {
+            var viewportPoint = Vector3.zero;
+
+            offset = Vector3.zero;
+
+            if (UnityEngine.Random.value > 0.5f)
+            {
+                var dir = Mathf.Round(UnityEngine.Random.value);
+                viewportPoint = new Vector3(dir, UnityEngine.Random.value);
+
+                offset = GetSpawnOffsetByViewportPosition(Vector3.right, dir < 0.001f ? -1f : 1f);
+            }
+            else
+            {
+                var dir = Mathf.Round(UnityEngine.Random.value);
+                viewportPoint = new Vector3(UnityEngine.Random.value, dir);
+
+                offset = GetSpawnOffsetByViewportPosition(Vector3.forward, dir < 0.001f ? -1f : 1f);
+            }
+
+            return viewportPoint;
         }
     }
 }
